e849d69840c29e6db4e66eb24cb55f5ad4e2bbdc
Ioanni-Balassis <1848800@students.wits.ac.za>
2021-06-16 14:43:58 +0200

    CHnage Colour

--- js/level3.js
+++ js/level3.js
@@ -102,7 +102,7 @@ function World() {
     scene = new THREE.Scene();
     fogDistance = 25000;
     scene.fog = new THREE.Fog(0xbadbe4, 1, fogDistance);
-    scene.background = new THREE.Color( 0x300067 );
+    // scene.background = new THREE.Color( 0x300067 );
 
     // Initialize the camera with field of view, aspect ratio,
     // near plane, and far plane.
